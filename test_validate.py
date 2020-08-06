3b4910d734bedf7eb5188b5c42dee5efb8203e03
benlen10 <ben2lening@gmail.com>
2020-08-06 15:28:49 -0700

    Implement pytest framework

---
+++ test_validate.py
@@ -0,0 +1,14 @@
+def test_validate_esma():
+    assert False
+
+
+def test_validate_aha():
+    assert False
+
+
+def test_validate_arc():
+    assert False
+
+
+def test_validate_dca():
+    assert False
